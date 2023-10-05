c884459479884d9667ff20b6e55aa9ad0e64f27d
Santos Hernandez <santos@zebedee.io>
2023-10-04 21:38:28 -0700

    fixed build issue

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
 import Image from 'next/image';
-import { ArrowRight } from 'react-bootstrap-icons';
-
 
 export default function Home() {
   return  (
@@ -133,7 +131,6 @@ export default function Home() {
         </footer>
       </div>
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </div>
   );
 }
